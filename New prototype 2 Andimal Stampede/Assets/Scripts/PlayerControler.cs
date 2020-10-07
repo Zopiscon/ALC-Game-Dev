7e0540ca402ecda6d3c5d7bc34e90bd42c7e6b1e
Kyle Scholl <kjs003@outlook.com>
2020-10-07 12:43:58 -0600

    Work on new prototype 2

---
+++ New prototype 2 Andimal Stampede/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        
+        //constrains leftward movement x @ -10
+        if(transform.position.x < -xRange);
+        { 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            
+        }
+        
+        //constrains rightward movement x @ 10
+        if(transform.position.x = xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        if(input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodProjectile, transform.position, foodProjectile.transfom.rotation);
+        }
+    }
+}
