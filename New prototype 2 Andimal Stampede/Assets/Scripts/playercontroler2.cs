c488e4b13cba52f7b672b728f92c0d451172cceb
Kyle Scholl <kjs003@outlook.com>
2020-10-09 12:39:00 -0600

    fixed scripts to play

--- New prototype 2 Andimal Stampede/Assets/Scripts/playercontroler2.cs
+++ New prototype 2 Andimal Stampede/Assets/Scripts/playercontroler2.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontrler2 : MonoBehaviour
+public class playercontroler2 : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10f;
+    public float xRange = 20f;
+    public GameObject foodMissile;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,23 @@ public class playercontrler2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // alows for left and right movement for character
+        horizontalInput = Input.GetAxis("horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        // limits x bounds for rightward movement
+        if(transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         
+        }
+        // limits x bounds for leftward movement
+        if(transform.position.x > -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodMissile, transform.position, foodMissile.transform.rotation);
+        }
     }
 }
