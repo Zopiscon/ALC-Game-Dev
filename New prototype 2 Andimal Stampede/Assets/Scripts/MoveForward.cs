2f6a7608048dd39ffd4a8c75d0570487e059ea0f
Kyle Scholl <kjs003@outlook.com>
2020-10-13 12:40:48 -0600

    code for animal spawn

--- New prototype 2 Andimal Stampede/Assets/Scripts/MoveForward.cs
+++ New prototype 2 Andimal Stampede/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 10f;
+    private float speed = 3f;
     
     // Start is called before the first frame update
     void Start()
