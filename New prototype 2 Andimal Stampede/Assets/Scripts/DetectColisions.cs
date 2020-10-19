c554c514a2e799db67b9f0352bcda863a8a5cadf
Kyle Scholl <kjs003@outlook.com>
2020-10-19 12:38:44 -0600

    food destroys randomly spawned animals

---
+++ New prototype 2 Andimal Stampede/Assets/Scripts/DetectColisions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectColisions : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy this object
+        Destroy(gameObject);
+        // Destroy other object this object is colliding with
+        Destroy(other.gameObject);
+    }
+}
--- New prototype 2 Andimal Stampede/Assets/Scripts/MoveForward.cs
+++ New prototype 2 Andimal Stampede/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    private float speed = 3f;
+    private float speed = 5f;
     
     // Start is called before the first frame update
     void Start()
